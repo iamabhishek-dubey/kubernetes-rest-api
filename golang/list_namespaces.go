8c241a8cf534a9a729de3c49088a3be12a5be372
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2019-04-29 15:01:10 +0530

    Added rest code for golang

---
+++ golang/list_namespaces.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	log.Println("Using kubeconfig file: ", kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	namespaces, err := clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
+
+	if err != nil {
+		log.Fatalln("failed to get pods:", err)
+	}
+
+	fmt.Println(namespaces)
+}
